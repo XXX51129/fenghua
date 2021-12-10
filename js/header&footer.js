2e4390b4b5857d8943633f1f3962f31f750664e1
谢晓雪 <1841762072@qq.com>
2021-12-10 13:24:03 +0800

    '准备工作3'

---
+++ js/header&footer.js
@@ -0,0 +1,15 @@
+let gengduo_ = document.getElementById('gengduo');
+let linkText_ = document.getElementById('linkText');
+
+
+let i = 0;
+// 单数点击时变色，显示内容
+touch.on(gengduo_, 'tap', function () {
+    if (i++ % 2 === 0) {
+        this.style.color = '#556B2F';
+        linkText_.style.display = 'block';
+    } else {
+        this.style.color = '#379818';
+        linkText_.style.display = 'none';
+    }
+});
